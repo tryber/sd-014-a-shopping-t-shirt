955c7cd5783752f57465e70faf96b89d83e08bfd
Hugo Ferreira <hugonardo@gmail.com>
2021-08-23 18:15:38 -0300

    fix eslint errors

--- js/lista-produtos.js
+++ js/lista-produtos.js
@@ -1,54 +1,34 @@
 let listagemDeProdutos = [];
 
-const divListagem = document.getElementById("listagem");
-const listaGenero = document.getElementById("genero");
-const listaOrdenacao = document.getElementById("ordem");
+const divListagem = document.getElementById('listagem');
+const listaGenero = document.getElementById('genero');
+const listaOrdenacao = document.getElementById('ordem');
 const filtro = document.getElementById('search');
 
 const btnFiltro = document.getElementById('btn-search');
 
-btnFiltro.addEventListener('click', ()=>{
-    filtrarProdutos(filtro.value);
-});
-
-const filtrarProdutos =  (Textofiltro) =>{
-    listagemDeProdutos = listagemDeProdutos.filter((produto)=> produto.title.toUpperCase().includes(Textofiltro.toUpperCase()));
-    preencheListaDeProdutos();
-}
-
-listaOrdenacao.addEventListener("change", async (event) => {
-  await ordenaPorPreco(event.target.value);
-});
-
-listaGenero.addEventListener("change", (event) => {
-  let genero = event.target.value;
-  console.log(genero);
-  filtro.value = '';
-  listaOrdenacao.value = 'relevancia';
-  buscaProdutos(genero);
-});
-
+// eslint-disable-next-line max-lines-per-function
 const listaProduto = (srcImagen, descricaoProduto, precoDoProduto) => {
-  const infoProduto = document.createElement("div");
-  const imagem = document.createElement("img");
-  const tituloProduto = document.createElement("h2");
-  const precoProduto = document.createElement("div");
+  const infoProduto = document.createElement('div');
+  const imagem = document.createElement('img');
+  const tituloProduto = document.createElement('h2');
+  const precoProduto = document.createElement('div');
 
-  infoProduto.classList.add("col-md-3");
-  infoProduto.classList.add("info-produto");
+  infoProduto.classList.add('col-md-3');
+  infoProduto.classList.add('info-produto');
 
-  imagem.classList.add("rounded");
-  imagem.classList.add("img_produto");
-  imagem.classList.add("img_fluid");
+  imagem.classList.add('rounded');
+  imagem.classList.add('img_produto');
+  imagem.classList.add('img_fluid');
   imagem.src = srcImagen;
 
-  tituloProduto.className = "desc_produto";
-  tituloProduto.innerText = descricaoProduto.slice(0, 37) + "...";
+  tituloProduto.className = 'desc_produto';
+  tituloProduto.innerText = `${descricaoProduto.slice(0, 37)}...`;
 
-  precoProduto.className = "preco_produto";
-  precoProduto.innerText = Intl.NumberFormat("pt-br", {
-    style: "currency",
-    currency: "BRL",
+  precoProduto.className = 'preco_produto';
+  precoProduto.innerText = Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL',
   }).format(precoDoProduto);
 
   infoProduto.appendChild(imagem);
@@ -58,32 +38,54 @@ const listaProduto = (srcImagen, descricaoProduto, precoDoProduto) => {
   divListagem.appendChild(infoProduto);
 };
 
-const ordenaPorPreco = (ordem) => {
-  if (ordem === "menor-preco") {
-    listagemDeProdutos.sort((a, b) => a.price - b.price);
-  }
-  if (ordem === "maior-preco") {
-    listagemDeProdutos.sort((a, b) => b.price - a.price);
-  }
-  preencheListaDeProdutos();
-};
-
 const preencheListaDeProdutos = () => {
-  divListagem.innerHTML = "";
+  divListagem.innerHTML = '';
   listagemDeProdutos.forEach((produto) => {
     listaProduto(produto.thumbnail, produto.title, produto.price);
   });
 };
 
+const filtrarProdutos = (Textofiltro) => {
+  listagemDeProdutos = listagemDeProdutos
+    .filter((produto) => produto.title.toUpperCase().includes(Textofiltro.toUpperCase()));
+  preencheListaDeProdutos();
+};
+
+btnFiltro.addEventListener('click', () => {
+  filtrarProdutos(filtro.value);
+});
+
+const ordenaPorPreco = (ordem) => {
+  if (ordem === 'menor-preco') {
+    listagemDeProdutos.sort((a, b) => a.price - b.price);
+  }
+  if (ordem === 'maior-preco') {
+    listagemDeProdutos.sort((a, b) => b.price - a.price);
+  }
+  preencheListaDeProdutos();
+};
+
+listaOrdenacao.addEventListener('change', async (event) => {
+  await ordenaPorPreco(event.target.value);
+});
+
 const buscaProdutos = async (genero) => {
   const listaDeProdutos = await fetch(
-    `https://api.mercadolibre.com/sites/MLB/search?q=${genero}`
+    `https://api.mercadolibre.com/sites/MLB/search?q=${genero}`,
   );
   const listaDeProdutosJson = await listaDeProdutos.json();
   listagemDeProdutos = listaDeProdutosJson.results;
   preencheListaDeProdutos();
 };
 
+listaGenero.addEventListener('change', (event) => {
+  const genero = event.target.value;
+  console.log(genero);
+  filtro.value = '';
+  listaOrdenacao.value = 'relevancia';
+  buscaProdutos(genero);
+});
+
 window.onload = () => {
-  buscaProdutos("camisas-infantil");
+  buscaProdutos('camisas-infantil');
 };
